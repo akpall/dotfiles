21b36ecc6e961add9845373d8549aee918f897d9
Allen-Kristjan Päll <allenkristjanpall@gmail.com>
2024-05-10 05:30:56 +0300

    home/dot-config/emacs/early-init.el: added

---
+++ home/dot-config/emacs/early-init.el
@@ -0,0 +1,15 @@
+;; boosts GC threshold for faster startup, then resets
+(setq
+ startup/gc-cons-threshold gc-cons-threshold
+ gc-cons-threshold most-positive-fixnum)
+(add-hook 'emacs-startup-hook
+	  #'(lambda ()
+	      (setq gc-cons-threshold startup/gc-cons-threshold)))
+
+;; disables menu, tool, and scroll bars
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+
+;; sets "Dina" as the default font
+(add-to-list 'default-frame-alist '(font . "Dina"))
