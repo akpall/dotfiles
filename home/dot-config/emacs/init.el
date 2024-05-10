7e9a1d51314fb4b6cdc1e0240d2d00214a8ae37b
Allen-Kristjan Päll <allenkristjanpall@gmail.com>
2024-05-10 05:32:37 +0300

    home/dot-config/emacs/init.el: added

---
+++ home/dot-config/emacs/init.el
@@ -0,0 +1,30 @@
+;; adds MELPA to package archives and initializes
+(require 'package)
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+
+;; loads use-package and sets default options
+(require 'use-package)
+(setq use-package-always-defer t
+      use-package-always-ensure t)
+
+;; creates and loads custom.el if it doesn't exist
+(setq custom-file
+      (expand-file-name "custom.el" user-emacs-directory))
+(unless (file-exists-p custom-file)
+  (write-region "" nil custom-file))
+(load custom-file nil t)
+
+;; ensures files end with a newline
+(setq require-final-newline t)
+
+;; loads all .el files in the "packages" directory
+(let
+    ((packages
+      (directory-files
+       (expand-file-name "packages" user-emacs-directory)
+       t "\\.el$")))
+  (dolist
+      (package packages)
+    (load package)))
