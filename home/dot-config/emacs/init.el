b216b553867f83d15b1c7d4ea9398e766113483c
Allen-Kristjan Päll <allenkristjanpall@gmail.com>
2024-05-11 06:54:58 +0300

    home/dot-config/emacs/init.el: reworked
    * comment changes
    
    * defined emacs-tmp-dir constant
    
    * set various settings under one setq
    
    * enable many modes

--- home/dot-config/emacs/init.el
+++ home/dot-config/emacs/init.el
@@ -1,26 +1,45 @@
-;; adds MELPA to package archives and initializes
+;; adds MELPA to the list of package archives and initializes
 (add-to-list 'package-archives
 	     '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
 
-;; loads use-package and sets default options
-(setq use-package-always-defer t
-      use-package-always-ensure t)
+;; defines a constant for the temporary directory path based on the user's UID
+(defconst emacs-tmp-dir (expand-file-name (format "%d/emacs" (user-uid)) "/run/user/"))
 
-;; creates and loads custom.el if it doesn't exist
-(setq custom-file
-      (expand-file-name "custom.el" user-emacs-directory))
+;; sets various settings
+(setq auto-save-file-name-transforms `((".*" ,emacs-tmp-dir t))
+      auto-save-list-file-prefix emacs-tmp-dir
+      backup-by-copying t
+      backup-directory-alist `((".*" . ,emacs-tmp-dir))
+      create-lockfiles nil
+      custom-file (expand-file-name "custom.el" user-emacs-directory)
+      delete-old-versions t
+      kept-new-versions 6
+      kept-old-versions 2
+      package-native-compile t
+      read-extended-command-predicate #'command-completion-default-include-p
+      require-final-newline t
+      tab-always-indent 'complete
+      use-package-always-defer t
+      use-package-always-ensure t
+      version-control t)
+
+;; creates custom file if it does not exist and loads it
 (unless (file-exists-p custom-file)
   (write-region "" nil custom-file))
 (load custom-file nil t)
 
-;; ensures files end with a newline
-(setq require-final-newline t)
-
+;; adds a hook to clean up whitespace before saving files
 (add-hook 'before-save-hook (lambda ()
 			      (whitespace-cleanup)))
 
-;; loads all .el files in the "packages" directory
+;; enable various modes
+(delete-selection-mode)
+(global-display-line-numbers-mode)
+(display-time-mode)
+(display-battery-mode)
+
+;; loads all .el files from the "packages" directory
 (let
     ((packages
       (directory-files
