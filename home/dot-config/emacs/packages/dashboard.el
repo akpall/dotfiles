fde868bcdc1fb6f4d12b0402dd6cdf7bbf386934
Allen-Kristjan Päll <allenkristjanpall@gmail.com>
2024-05-24 12:53:13 +0300

    home/dot-config/emacs/packages/dashboard.el: open dashboard by default

--- home/dot-config/emacs/packages/dashboard.el
+++ home/dot-config/emacs/packages/dashboard.el
@@ -3,5 +3,6 @@
   (emacs-startup . (lambda ()
 		     (dashboard-refresh-buffer)))
   :init
-  (setq dashboard-center-content t)
+  (setq dashboard-center-content t
+	initial-buffer-choice (lambda () (get-buffer-create dashboard-buffer-name)))
   (dashboard-setup-startup-hook))
